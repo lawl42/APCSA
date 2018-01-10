660caee8aa375eed4509eae41f6c7b7533e2b84e
Lawrence <lgwong168@gmail.com>
2018-01-10 09:37:31 -0800

    Update heater.java

--- heater.java
+++ heater.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class heater here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Lawrece
+ * @version 1.0
  */
 public class heater
 {
