0b563192709b6f6ffe3da1b7f6f08990b6c57def
Lawrence <lgwong168@gmail.com>
2018-01-20 19:19:33 -0800

    Update heater.java

--- Chapter 2/heater.java
+++ Chapter 2/heater.java
@@ -1,59 +1,59 @@
-
-  Write a description of class heater here.
- 
-  @author Lawrece
-  @version 1.0
- 
+/**
+ * Write a description of class heater here.
+ *
+ * @author Lawrence
+ * @version 1.0
+ */
 public class heater
 {
-     instance variables - replace the example below with your own
+    // instance variables - replace the example below with your own
     double temperature;
     double min, max, increment;
 
-    
-      Constructor for objects of class heater
-     
+    /**
+     * Constructor for objects of class heater
+     */
     public heater(double MIN_TEMP, double MAX_TEMP)
     {
-         initialise instance variables
+        // initialise instance variables
         temperature = 15.0;
         min = MIN_TEMP;
         max = MAX_TEMP;
         increment = 5.0;
     }
 
-    
-      raise temp
-     
+    /**
+     * raise temp
+     */
     public void warmer()
     {
         temperature += increment;
-        if (temperature  max) temperature = max;
+        if (temperature > max) temperature = max;
     }
     
-    
-      lower temp
-     
+    /*
+     * lower temp
+     */
     public void cooler()
     {
         temperature -= increment;
-        if (temperature  min) temperature = min;
+        if (temperature < min) temperature = min;
     }
     
-    
-      get temp
-     
+    /*
+     * get temp
+     */
     public double getTemp()
     {
         return temperature;
     }
     
-    
-      set inc
-     
+    /*
+     * set inc
+     */
     public void setInc(double newInc)
     {
-        if (newInc  0) System.out.println(Give a non-negative increment.);
+        if (newInc < 0) System.out.println("Give a non-negative increment.");
         else increment = newInc;
     }
 }
